2417fd9d53cfada5be7e3529137b8c34a0f077f7
victormorgado <victormorgado@gmail.com>
2024-12-05 21:42:05 -0500

    todo list2

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -35,5 +35,11 @@ function clearCompletedTasks() {
     displayTasks();
 }
 
+function clearAllTasks() {
+    tasks = [];
+    displayTasks();
+}
+
 addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+clearAllBtn.addEventListener("click", clearAllTasks);
